93a87eaa87fefb81127d4bbdf9168bda3cdb22b3
DevExpressExampleBot <DevExpressExampleBot@users.noreply.github.com>
2018-04-23 19:21:59 +0300

    initial commit

---
+++ CS/MainWindow.xaml.cs
@@ -0,0 +1,38 @@
+﻿using System.Windows;
+using System.Windows.Media;
+using DevExpress.Xpf.Gauges;
+
+namespace DXGauges_RangeEvents {
+
+    public partial class MainWindow : Window {
+        public MainWindow() {
+            InitializeComponent();
+        }
+
+        private void GreenRange_IndicatorEnter(object sender, IndicatorEnterEventArgs e) {
+            stateIndicator.Fill = new SolidColorBrush(Colors.Green);
+        }
+
+        private void GreenRange_IndicatorLeave(object sender, IndicatorLeaveEventArgs e) {
+            stateIndicator.Fill = new SolidColorBrush(Colors.Gray);
+        }
+
+        private void YellowRange_IndicatorEnter(object sender, IndicatorEnterEventArgs e) {
+            stateIndicator.Fill = new SolidColorBrush(Colors.Yellow);
+        }
+
+        private void YellowRange_IndicatorLeave(object sender, IndicatorLeaveEventArgs e) {
+            if (((e.NewValue) < 10) || (((e.NewValue) > 90)))
+                stateIndicator.Fill = new SolidColorBrush(Colors.Gray);
+        }
+
+        private void RedRange_IndicatorEnter(object sender, IndicatorEnterEventArgs e) {
+            stateIndicator.Fill = new SolidColorBrush(Colors.Red);
+        }
+
+        private void RedRange_IndicatorLeave(object sender, IndicatorLeaveEventArgs e) {
+            if (((e.NewValue) < 10) || (((e.NewValue) > 90)))
+                stateIndicator.Fill = new SolidColorBrush(Colors.Gray);
+        }
+    }
+}
